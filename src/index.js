2d9f0dda222bb0367ca1e931b1cfe00951527d9a
tocology <hwangjun7777@gmail.com>
2016-02-21 23:45:27 +0900

    feat: Modify search bar

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom'
 
 import { Youtube_API } from '../config/config'
 
+import SearchBar from './components/search_bar'
+
 // Create a new component. This componet should produce some HTML
 const App = () => {
-    return <div>Hi!</div>
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    )
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
