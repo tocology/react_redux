bf80f59d8e77e493fe29b1ef48b75c683ee91ebd
tocology <hwangjun7777@gmail.com>
2016-02-21 23:48:26 +0900

    feat: Modify Key

--- config/config.js
+++ config/config.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 export const Youtube_API = "AIzaSyAgT6HTo9dTS55qY86NQCP5FsJf13sq2yc"
-=======
-export const Youtube_API  = "write down you key";
->>>>>>> 2b432b76efdc207b7de4476e206b4bb74d17e759
