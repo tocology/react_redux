e58d9a154191573e21c547eaee3554c658ee50de
JunHo Hwang <hwangjun7777@gmail.com>
2016-02-23 13:46:54 +0000

    feat: Modify index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import YTSearch from 'youtube-api-search'
 
 import { Youtube_API } from '../config/config'
 
 import SearchBar from './components/search_bar'
 
+YTSearch({key: Youtube_API, term: 'surfboards'}, function(data){
+    console.log(data)
+})
+
 // Create a new component. This componet should produce some HTML
-const App = () => {
-    return (
+class App extends Component {
+    constructor(props) {
+        super(props)
+        
+        this.state = { videos: [] }
+    }
+    
+    render() {
+        return (
         <div>
             <SearchBar />
         </div>
-    )
+        )
+    }
 }
 
 // Take this component's generated HTML and put it on the page (in the DOM)
