611bc8bbffe55d8cea35babf2eae9ff73d9ac621
tocology <hwangjun7777@gmail.com>
2016-02-25 00:48:53 +0900

    feat: Add video details function

--- src/index.js
+++ src/index.js
@@ -6,16 +6,23 @@ import { Youtube_API } from '../config/config'
 
 import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail'
 
 // Create a new component. This componet should produce some HTML
 class App extends Component {
     constructor(props) {
         super(props)
         
-        this.state = { videos: [] }
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        }
 
         YTSearch({key: Youtube_API, term: 'surfboards'}, (videos) => {
-            this.setState({ videos }) // { videos : videos }
+            this.setState({
+                videos: videos,
+                selectedVideo: videos[0]
+            }) // { videos : videos }
         })
     }
     
@@ -23,6 +30,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video={this.state.selectedVideo} />
                 <VideoList videos={this.state.videos}/>
             </div>
         )
