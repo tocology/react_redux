e7d3abf8fdd9e58b9e56740c86dc88bea010b968
tocology <hwangjun7777@gmail.com>
2016-03-07 00:00:34 +0900

    feat: Add SearchBar to WeatherForecast App

--- config/config.js
+++
@@ -1,1 +0,0 @@
-export const Youtube_API = "AIzaSyAgT6HTo9dTS55qY86NQCP5FsJf13sq2yc"
--- src/components/app.js
+++ src/components/app.js
@@ -1,3 +1,13 @@
-/**
- * Created by JunHo on 2016. 3. 6..
- */
+import React, { Component } from 'react'
+
+import SearchBar from '../containers/search_bar'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        )
+    }
+}
