5c7f8d3144a04b19dd35dec65440641f03b878ca
tocology <hwangjun7777@gmail.com>
2016-03-08 23:20:26 +0900

    feat: Add actions & reduces

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,15 @@
 import axios from 'axios'
-import { WEATHER_API } from "../../config/config"
+import { Weather_API } from "../../config/config"
 
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${WEATHER_API}`
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${Weather_API}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
 export function fetchWeather(city){
     const url = `${ROOT_URL}&q=${city},us`
     const request = axios.get(url)
+
+    console.log('Request: ', request)
 
     return {
         type: FETCH_WEATHER,
